c38f012aa70257638bc0d6392a3fbe439cdcc3ef
Evgeni <bikovevg@gmail.com>
2022-01-28 13:47:33 +0000

    non-triavial flow added

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ engine = create_engine('sqlite:///bot-converter.db', echo=True)
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'users'
+from sqlalchemy import Column, Integer, String
+class File(Base):
+    __tablename__ = 'files'
 
     id = Column(Integer, primary_key=True)
     chat_id = Column(Integer)
     filename = Column(String)
 
     def __repr__(self):
-        return "<User(chat_id='{}', filename='{}')>".format(self.chat_id, self.filename)
+        return "<File(chat_id='{}', filename='{}')>".format(self.chat_id, self.filename)
+    
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer)
+    username = Column(String)
+    state = Column(String)
+
+    def __repr__(self):
+        return "<User(chat_id='{}', username='{}')>".format(self.chat_id, self.filename)
 
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///bot-converter.db', echo=True)
@@ -28,12 +40,27 @@ from telebot import types
 bot = telebot.TeleBot("5069674898:AAGdHYM4cn-ZqQqbhR2vke34ke8qMos1lTY", parse_mode=None)
 
 WELCOME_MSG = "👋 Howdy, how are you doing? \
-I can convert video to the following formats: *.avi, *.mov, *.mp4! and convert to *.avi, *.mov, *.mp4, *.gif!\
-Just upload a short video for me, and then choose file type you want."
+You can upload video with the following formats: *.avi, *.mov, *.mp4! and I convert them to *.avi, *.mov, *.mp4, *.gif! \
+Just upload a short video to me, and then choose file type you want. \
+Following commands are avilable: /start, /setname, /help!"
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, WELCOME_MSG)
+    bot.reply_to(message, WELCOME_MSG)
+    ed_user = User(chat_id=message.chat.id, state='start')
+    session = Session()
+    session.add(ed_user)
+    session.commit()
+    session.close()
+    
+@bot.message_handler(commands=['setname'])
+def set_name(message):
+    bot.reply_to(message, 'Set your username')
+    session = Session()
+    our_user = session.query(User).filter_by(chat_id=message.chat.id).order_by(User.id.desc()).first()
+    our_user.state = 'set_name'
+    session.commit()
+    session.close()
 
 @bot.message_handler(content_types=['video', 'video_note', 'document'])
 def handle_docs_video(message):
@@ -71,9 +98,9 @@ def handle_docs_video(message):
     markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
     bot.send_message(message.chat.id, "Choose the target file type:", reply_markup=markup)
     
-    ed_user = User(chat_id=message.chat.id, filename=temp_filename)
+    ed_file = File(chat_id=message.chat.id, filename=temp_filename)
     session = Session()
-    session.add(ed_user)
+    session.add(ed_file)
     session.commit()
     session.close()
 
@@ -85,10 +112,10 @@ def process_file(message):
     bot.send_message(message.chat.id, 'You\'ve chosen .{} format! Processing...'.format(target_file_type), reply_markup=markup)
     
     session = Session()
-    our_user = session.query(User).filter_by(chat_id=message.chat.id).order_by(User.id.desc()).first()
+    our_file = session.query(File).filter_by(chat_id=message.chat.id).order_by(File.id.desc()).first()
     session.close()
     
-    temp_filename = our_user.filename
+    temp_filename = our_file.filename
     
     # Convert file
     import ffmpeg
@@ -103,13 +130,27 @@ def process_file(message):
     
     # Send file
     with open(output_name,'rb') as converted_file:
+        session = Session()
+        our_user = session.query(User).filter_by(chat_id=message.chat.id).order_by(User.id.desc()).first()
+        session.close()
+        if our_user.username:
+            bot.send_message(message.chat.id, 'Thank you for using our service, {}'.format(our_user.username))
         bot.send_document(message.chat.id, converted_file)
-        import os
+    import os
     os.remove(output_name)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    session = Session()
+    our_user = session.query(User).filter_by(chat_id=message.chat.id).order_by(User.id.desc()).first()
+    
+    if our_user.state == 'set_name':
+        our_user.username = message.text
+        our_user.state = 'complete'
+        session.commit()
+        bot.reply_to(message, 'Hello, {}, uppload a video you want to convert!'.format(our_user.username))
+        session.close()
+    else:
+        bot.reply_to(message, WELCOME_MSG)
 
 bot.infinity_polling()
-
